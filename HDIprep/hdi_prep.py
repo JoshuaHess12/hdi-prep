f8e118f50b8c890affdfea38be5ff3585d8c2b09
JoshuaHess12 <joshmhess12@gmail.com>
2020-05-05 12:56:07 -0400

    remove files

---
+++ HDIprep/hdi_prep.py
@@ -0,0 +1,10 @@
+#Module for high-dimensional imaging data importing
+#Developer: Joshua M. Hess, BSc
+#Developed at the Vaccine & Immunotherapy Center, Mass. General Hospital
+
+#Import external modules
+from pathlib import Path
+import os
+
+#Import custom modules
+from HDIimport import hdi_reader
